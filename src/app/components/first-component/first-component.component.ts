8489e8010c8826f3eb7f10cf5c7a22e90802d54a
Lucas Nolon Kochenburger <lucasnolon1234@gmail.com>
2023-05-07 18:21:19 -0300

    Add interpolação de dados no meu primeiro component

--- src/app/components/first-component/first-component.component.ts
+++ src/app/components/first-component/first-component.component.ts
@@ -3,13 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-first-component',
   templateUrl: './first-component.component.html',
-  styleUrls: ['./first-component.component.css']
+  styleUrls: ['./first-component.component.css'],
 })
 export class FirstComponentComponent implements OnInit {
+  name: string = 'Lucas';
+  age: number = 23;
+  job: string = 'Programador';
+  hobbies = ['Jogar', 'Estudar', 'Lutar'];
+  car = {
+    name: 'Clio',
+    year: 2013,
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
