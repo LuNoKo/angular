87e6ec321b79ed56f2c7c454aefe42194034ea4c
Lucas Nolon Kochenburger <lucasnolon1234@gmail.com>
2023-06-30 21:49:41 -0300

    Add Requisições HTTP

--- src/app/services/list.service.ts
+++ src/app/services/list.service.ts
@@ -1,13 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
 import { Animal } from '../interfaces/Animals';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ListService {
-  constructor() {}
+  private apiUrl = 'http://localhost:3000/animals';
+
+  constructor(private http: HttpClient) {}
 
   remove(animais: Animal[], animal: Animal) {
     return animais.filter((a) => animal.name != a.name);
+  }
+
+  getAll(): Observable<Animal[]> {
+    return this.http.get<Animal[]>(this.apiUrl);
   }
 }
