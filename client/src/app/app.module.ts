c213d9ab466fd768822d8f76aefe82e3001383a8
Lucas Nolon Kochenburger <lucasnolon1234@gmail.com>
2023-07-01 16:58:03 -0300

    Fix project

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,9 +5,19 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './page/home/home.component';
+import { AboutComponent } from './page/about/about.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
