6b47615b3a784dd5815f23697f005cbd580d6389
Lucas Nolon Kochenburger <lucasnolon1234@gmail.com>
2023-07-03 21:18:32 -0300

    Add style moment-form

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { NewMomentsComponent } from './pages/new-moments/new-moments.component';
+import { MomentFormComponent } from './components/moment-form/moment-form.component';
 
 @NgModule({
   declarations: [
@@ -19,6 +20,7 @@ import { NewMomentsComponent } from './pages/new-moments/new-moments.component';
     HomeComponent,
     AboutComponent,
     NewMomentsComponent,
+    MomentFormComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
