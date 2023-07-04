af46f2e0247bc4703d2397a5a283840c98919cc5
Lucas Nolon Kochenburger <lucasnolon1234@gmail.com>
2023-07-04 20:50:19 -0300

    Add criação de moment no banco da api

--- client/src/app/pages/new-moments/new-moments.component.ts
+++ client/src/app/pages/new-moments/new-moments.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Moment } from 'src/app/interfaces/Moment';
+import { MomentService } from 'src/app/services/moment/moment.service';
 
 @Component({
   selector: 'app-new-moments',
@@ -8,7 +10,26 @@ import { Component, OnInit } from '@angular/core';
 export class NewMomentsComponent implements OnInit {
   btnText = 'Compartilhar!';
 
-  constructor() {}
+  constructor(private momentService: MomentService) {}
 
   ngOnInit(): void {}
+
+  async createHandler(moment: Moment) {
+    const formData = new FormData();
+
+    formData.append('title', moment.title);
+    formData.append('description', moment.description);
+
+    if (moment.image) {
+      formData.append('image', moment.image);
+    }
+
+    // todo
+
+    await this.momentService.createMoment(formData).subscribe();
+
+    // -> Exibir msg
+
+    // -> Redirect
+  }
 }
