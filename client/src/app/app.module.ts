5274cd6c718d4ce80a61b3d1d5e3ce452ba6c2c7
Lucas Nolon Kochenburger <lucasnolon1234@gmail.com>
2023-07-03 21:33:32 -0300

    Add interfaces e services

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
@@ -22,7 +23,13 @@ import { MomentFormComponent } from './components/moment-form/moment-form.compon
     NewMomentsComponent,
     MomentFormComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
