820a5641f2b78233b6d32437bfd7df5663f7b4b6
Lucas Nolon Kochenburger <lucasnolon1234@gmail.com>
2023-07-08 20:34:13 -0300

    Add getMoments da API

--- client/src/app/services/moment/moment.service.ts
+++ client/src/app/services/moment/moment.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { Moment } from 'src/app/interfaces/Moment';
+import { Response } from 'src/app/interfaces/Response';
 
 @Injectable({
   providedIn: 'root',
@@ -10,6 +12,10 @@ export class MomentService {
   private apiUrl = `${this.baseApiUrl}api/moments`;
 
   constructor(private http: HttpClient) {}
+
+  getMoments() {
+    return this.http.get<Response<Moment[]>>(this.apiUrl);
+  }
 
   createMoment(formData: FormData) {
     return this.http.post<FormData>(this.apiUrl, formData);
