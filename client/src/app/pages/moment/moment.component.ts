58883b977c3f66a770a89f798b083d7f5111ed2e
Lucas Nolon Kochenburger <lucasnolon1234@gmail.com>
2023-07-16 16:04:22 -0300

    Add momento

--- client/src/app/pages/moment/moment.component.ts
+++ client/src/app/pages/moment/moment.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import { faEdit, faTimes } from '@fortawesome/free-solid-svg-icons';
 import { Moment } from 'src/app/interfaces/Moment';
 import { MomentService } from 'src/app/services/moment/moment.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-moment',
@@ -9,7 +11,11 @@ import { MomentService } from 'src/app/services/moment/moment.service';
   styleUrls: ['./moment.component.css'],
 })
 export class MomentComponent implements OnInit {
+  baseApiUrl = environment.baseApiUrl;
   moment?: Moment;
+
+  faTimes = faTimes;
+  faEdit = faEdit;
 
   constructor(
     private momentService: MomentService,
